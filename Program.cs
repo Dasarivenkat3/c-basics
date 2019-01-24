4856c15b1a761a748d6067643061594516f0da6d
Dasarivenkat03 <dasari.sai24@gmail.com>
2019-01-24 15:36:30 -0600

    1st commit

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace w01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int a = 0;
+             char b;
+             bool bl = false;
+             double d = 0.0;
+             string s = null;
+             decimal e;
+
+
+            Console.WriteLine("Hello World!");
+             
+             Console.WriteLine("enter your name");
+             string x  =  Console.ReadLine();
+
+             Console.WriteLine("enter a celcius value 0 t0 50");
+
+              a = int.Parse(Console.ReadLine());
+
+             
+
+            while ( true)
+            {
+              if(a>0 && a<=50)
+              {
+                   
+                    Console.WriteLine("select 'f' for farenheit and 'k' for kelvin and 'b' for both");
+                    char t = char.Parse(Console.ReadLine());
+                   switch (t)
+              {
+                case 'f':
+                      d = a * 1.8 + 32 ;
+                     Console.WriteLine("temperature in farenheit " + d);
+                    break;
+                case 'k':
+                      d = a + 273.15;
+
+                    Console.WriteLine(" temperature in kelvin " + d);
+                    break;
+                case 'b':
+                         d = a * 1.8 + 32 ;
+                         double m = a + 273.15;
+                     Console.WriteLine(" hell0 {0},temperature in farenheit is {1},temperature in kelvin {2}",x,d,m);
+                         break;
+                         
+                default:
+                    Console.WriteLine("select 'f' for farenheit and 'k' for kelvin and 'b' for both");
+                    break;
+      }
+
+                   
+              }
+              else
+              {
+                  Console.WriteLine("selected value out of range");
+              }
+
+           
+            }
+            
+
+        }
+
+    }
+}
